5003847ec735911dc9dd2c8b05c310ebc93ee5c2
Massimo Bonanni <massimo.bonanni@tiscali.it>
2019-06-07 10:26:39 +0200

    Added FinalizeOrder function

---
+++ Chapter07/OrderManager/OrderManager.Core/Entities/OrderStateChange.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderManager.Core.Entities
+{
+    public class OrderStateChange
+    {
+        public string OrderId { get; set; }
+        public OrderStatus NewOrderState { get; set; }
+    }
+}
---
+++ Chapter07/OrderManager/OrderManager.Functions/Activities/FinalizeOrder.cs
@@ -0,0 +1,65 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+using OrderManager.Core;
+using OrderManager.Core.Entities;
+using OrderManager.Functions.Extensions;
+
+namespace OrderManager.Functions.Activities
+{
+    public static class FinalizeOrder
+    {
+        [FunctionName(FunctionNames.FinalizeOrderFunction)]
+        public static async Task<bool> Run([ActivityTrigger] OrderStateChange orderStateChange,
+            [Table(SourceNames.OrdersTable, Connection = "StorageAccount")] CloudTable orderTable,
+            ILogger log)
+        {
+            log.LogInformation($"[START ACTIVITY] --> {FunctionNames.FinalizeOrderFunction} for OrderUd={orderStateChange.OrderId}");
+            bool retVal = true;
+            Order order = null;
+
+            (order, retVal) = await GetOrderFromTableAsync(orderStateChange, orderTable, log);
+            
+            if (retVal)
+            {
+                if (order != null)
+                {
+                    order.State = orderStateChange.NewOrderState;
+                    retVal = await orderTable.InsertOrReplaceAsync(order);
+                }
+                else
+                {
+                    log.LogWarning($"The Order {orderStateChange.OrderId} doesn't exist in the storage");
+                    retVal = false;
+                }
+            }
+
+            return retVal;
+        }
+
+        private static async Task<(Order order, bool result)> GetOrderFromTableAsync(OrderStateChange orderStateChange,
+            CloudTable orderTable, ILogger log)
+        {
+            var result = true;
+            Order order = null;
+            try
+            {
+                order = await orderTable.GetOrderByIdAsync(orderStateChange.OrderId);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, $"Error during retriving Order {orderStateChange.OrderId}");
+                result = false;
+            }
+
+            return (order, result);
+        }
+    }
+}
