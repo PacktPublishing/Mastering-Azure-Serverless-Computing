ddb91e758c7f21bf86e3ab8ddb75c5503c8f0426
Massimo Bonanni <mabonann@microsoft.com>
2019-11-30 17:32:28 +0100

    Added project for Durable Entities

---
+++ Chapter08/DurableEntities/DurableEntities/Counter.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureDayReloaded
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Counter
+    {
+        [JsonProperty("value")]
+        public int Value { get; set; }
+
+        [JsonProperty("lastUpdate")]
+        public DateTime? LastUpdate { get; set; }
+
+        public void Add(int amount)
+        {
+            this.Value += amount;
+            this.LastUpdate = DateTime.UtcNow;
+        }
+
+        public Task Reset()
+        {
+            this.Value = 0;
+            this.LastUpdate = DateTime.UtcNow;
+            return Task.CompletedTask;
+        }
+
+        public Task<int> GetValue()
+        {
+            return Task.FromResult(this.Value);
+        }
+
+        public void Delete()
+        {
+            Entity.Current.DeleteState();
+        }
+
+        [FunctionName(nameof(Counter))]
+        public static Task Run([EntityTrigger] IDurableEntityContext ctx)
+        {
+            ctx.DispatchAsync<Counter>();
+
+            return Task.CompletedTask;
+        }
+
+    }
+}
---
+++ Chapter08/DurableEntities/DurableEntities/CounterFunctions.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureDayReloaded
+{
+    public class CounterFunctions
+    {
+        [FunctionName("DeleteCounter")]
+        public async Task<HttpResponseMessage> DeleteCounter(
+            [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "Counter/{entityKey}")] HttpRequestMessage req,
+            [DurableClient] IDurableEntityClient client,
+            string entityKey)
+        {
+            var entityId = new EntityId("Counter", entityKey);
+
+            await client.SignalEntityAsync(entityId, "Delete");
+
+            return req.CreateResponse(HttpStatusCode.Accepted);
+        }
+
+        [FunctionName("GetCounter")]
+        public static async Task<HttpResponseMessage> GetCounter(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "Counter/{entityKey}")] HttpRequestMessage req,
+            [DurableClient] IDurableEntityClient client,
+            string entityKey)
+        {
+            var entityId = new EntityId("Counter", entityKey);
+
+            var state = await client.ReadEntityStateAsync<Counter>(entityId);
+
+            return req.CreateResponse(state);
+        }
+
+        [FunctionName("IncrementCounter")]
+        public static async Task<HttpResponseMessage> IncrementCounter(
+            [HttpTrigger(AuthorizationLevel.Function, "put", Route = "Counter/{entityKey}/{increment?}")] HttpRequestMessage req,
+            [DurableClient] IDurableEntityClient client,
+            string entityKey,
+            int? increment)
+        {
+            var entityId = new EntityId("Counter", entityKey);
+            if (!increment.HasValue)
+                increment = 1;
+
+            await client.SignalEntityAsync(entityId, "Add", increment.Value);
+
+            return req.CreateResponse(HttpStatusCode.Accepted);
+        }
+    }
+}
